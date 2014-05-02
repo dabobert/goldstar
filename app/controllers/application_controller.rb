0c4200bbdc86539ab3da4f78dd926ee1d1df9f50
Robert Jenkins <bobert@gmail.com>
2014-05-02 15:25:42 -0400

    - resolved parse errors caused by rails 3 syntax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,49 +1,12 @@
 class ApplicationController < ActionController::Base
-  before_filter :generate_symbols
   protect_from_forgery
   
+
+
+
   protected
   
   def store_page
     session[:return_to] ||= request.referer
-  end
-  
-  def generate_symbols
-    hollow_star
-    filled_star
-    gold_star
-    grey_heart
-    red_heart
-    heart
-  end
-
-  def heart
-    #the heart character is displayed wierd in text mate, which is why i use the entity. ♥
-    #i don't trust raw special chars in all enviorments, editors, ides's and os's
-    @heart ||= "&#9829;"
-  end
-  
-  def grey_heart
-    @grey_heart ||= '<span style="color:grey">&#9829;</span>'
-  end
-  
-  def red_heart
-    @red_heart ||= '<span style="color:red">&#9829;</span>'
-  end
-  
-  def hollow_star
-    @hollow_star ||= '<span style="color:gold">&#9734;</span>'
-  end
-  
-  def gold_star
-    @gold_star ||= filled_star
-  end
-  
-  def filled_star
-    @filled_star ||= '<span style="color:gold">&#9733;</span>'
-  end
-  
-  def get_twitter_keys
-    @twitter_config = YAML.load(File.read(Rails.root.join('config', 'twitter.yml')))[Rails.env]
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
   end
   
   def render_new_fav star
-    raw link_to (heart, star_favorites_path(star), :class=>"new_fav", :method=>:post)
+    raw(link_to(heart, star_favorites_path(star), :class=>"new_fav", :method=>:post))
   end
 
   def heart
@@ -15,23 +15,23 @@ module ApplicationHelper
   end
   
   def grey_heart
-    @grey_heart ||= raw '<span style="color:grey">&#9829;</span>'
+    @grey_heart ||= raw('<span style="color:grey">&#9829;</span>')
   end
   
   def red_heart
-    @red_heart ||= raw '<span style="color:red">&#9829;</span>'
+    @red_heart ||= raw('<span style="color:red">&#9829;</span>')
   end
   
   def hollow_star
-    @hollow_star ||= raw '<span style="color:gold">&#9734;</span>'
+    @hollow_star ||= raw('<span style="color:gold">&#9734;</span>')
   end
   
   def gold_star
-    @gold_star ||= raw filled_star
+    @gold_star ||= raw(filled_star)
   end
   
   def filled_star
-    @filled_star ||= raw '<span style="color:gold">&#9733;</span>'
+    @filled_star ||= raw('<span style="color:gold">&#9733;</span>')
   end
 
 
